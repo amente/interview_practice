6edcfe68bd1323034fbd3036bfa97469faaba434
hack-am <amenteknows@gmail.com>
2013-11-29 15:15:10 -0500

    Implemented tail, remove,length for LL

--- linkedlist.c
+++ linkedlist.c
@@ -3,30 +3,33 @@
 
 /* Representation of a LinkedList Node */
 struct Node{
-	struct Node * next;
+	struct Node *next;
 	int value;
 };
 
 /* Representation of a LinkedList */
 typedef struct {
-	struct Node * head;	
+	struct Node *head;	
+	struct Node *tail;
+	int length;
 }LinkedList;
 
 
-
-
 /* Creates a new LinkedList */
-LinkedList * linkedList(){    
-	LinkedList * l = malloc(sizeof(LinkedList));	
+LinkedList *LL_linkedList(){    
+	LinkedList *l = malloc(sizeof(LinkedList));	
+    if(!l){exit(0);} //ERROR
 	l->head = NULL;
+	l->tail = NULL;
 	return l;
 }
 
 /* Destroys a LinkedList */
-void destroy(LinkedList *l){
-	struct Node * cur = l->head;
+void LL_destroy(LinkedList *l){
+	printf("Destroying\n");
+	struct Node *cur = l->head;
 	while(cur!=NULL){
-		struct Node * next = cur->next;
+		struct Node *next = cur->next;		
 		free(cur);
 		cur = next;
 	}
@@ -34,21 +37,67 @@ void destroy(LinkedList *l){
 }
 
 /* Appends a new Node to the end of the LinkedList */
-void add(LinkedList * l,int value){ 
-	struct Node * cur = l->head;
+void LL_add(LinkedList *l,int value){ 
+	struct Node *cur = l->head;
+	struct Node *new = (struct Node *)malloc(sizeof(struct Node));
+	if(!new){exit(0);} //ERROR
+	new->next = NULL;
+	new->value = value;	
 	if(cur==NULL){
-	  l->head = (struct Node *)malloc(sizeof(struct Node));
-	  l->head->value = value;
+	  l->head = new;
 	}else{	
 		while(cur->next!=NULL){
 			cur = cur->next;
-		}
-		struct Node * new = (struct Node *)malloc(sizeof(struct Node));
-		new->next = NULL;
-		new->value = value;	
+		}		
 		cur->next = new;
-	}	
+	}
+	l->tail = new;
+	l->length++;	
 }
+
+/* Returns the length of the LinkedList*/
+int LL_length(LinkedList *l){	
+	return l->length;	
+}
+
+/* Removes the first occurence of a value from a LinkedList */
+int LL_remove(LinkedList *l,int value){
+	struct Node *cur = l->head;
+	struct Node *prev = NULL;
+	while(cur!=NULL){	
+		if(cur->value==value){
+			if(l->head==cur){
+				//Removing head
+				l->head=cur->next;				
+			}else if(l->tail==cur){
+				//Removing tail				
+				l->tail=prev;	
+				prev->next = NULL;				
+			}else{
+				//Removing normal Node
+				prev->next = cur->next;						
+			}
+			l->length--;
+			free(cur);
+			return 1;
+		}else{
+			prev = cur;
+			cur = cur->next;
+		}		
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- testLinkedlist.c
+++ testLinkedlist.c
@@ -4,10 +4,23 @@
 
 
 void main(void){    
-	LinkedList *l=linkedList();	
-    add(l,10);
-	add(l,20);
-	printf("%d\n",l->head->value);
-	printf("%d\n",l->head->next->value);
-	free(l);
+	LinkedList *l=LL_linkedList();	
+	int i;
+	for(i=0;i<10;i++){
+	    printf("Added %d\n",i);
+		LL_add(l,i);
+	}	
+	printf("Length: %d\n",LL_length(l));
+	printf("Head: %d\n",l->head->value);
+	printf("Tail: %d\n",l->tail->value);
+	printf("Remove: %d\n",5);
+	LL_remove(l,5);
+	printf("Length: %d\n",LL_length(l));
+	printf("Remove Head: %d\n",l->head->value);
+	LL_remove(l,l->head->value);
+	printf("Length: %d\n",LL_length(l));
+	printf("Remove Tail: %d\n",l->tail->value);
+	LL_remove(l,l->tail->value);
+	printf("Length: %d\n",LL_length(l));
+	LL_destroy(l);
 }
